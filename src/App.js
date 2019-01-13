e30a54a764b50cc611031730ec72403edf22d3d4
LaurenceKrall <laurencekrall@gmail.com>
2019-01-13 18:06:22 +0000

    test azure deploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello world 2 again
+            Hello world 2 again again
           </p>
           <a
             className="App-link"
