6fbdc71c2b3faf50bed3e8b58a89a1668d7314a0
laurencekrall <laurence_krall@hotmail.com>
2019-01-19 23:43:56 +0000

    get data from api, add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component<{}, {word: string}> {
+class App extends Component<{}, {word: string, list : string[]}> {
   constructor(props : any) {
     super(props);
     this.state = {
-      word : "hello world"
-    };
+      word : "hello world",
+      list : ["a", "b"]
+    }
   }
 
   render() {
+    axios.get('https://laurenceazure.azurewebsites.net/api/values')
+    .then(json => this.setState({
+      word : "hello world",
+      list : json.data
+    }))
+
+    const moves = this.state.list.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>{step}</li>
+      );
+    });
+
     return (
       <div className="App">
         <header className="App-header">
@@ -25,6 +42,7 @@ class App extends Component<{}, {word: string}> {
           >
             Learn {this.state.word}
           </a>
+          <ol>{moves}</ol>
         </header>
       </div>
     );
