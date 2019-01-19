6af615efa86ae44a93d55297ee61531c73a42112
laurencekrall <laurence_krall@hotmail.com>
2019-01-19 21:15:17 +0000

    convert to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component<{}, {word: string}> {
+  constructor(props : any) {
+    super(props);
+    this.state = {
+      word : "hello world"
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello world 2 again 2
+            Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn {this.state.word}
           </a>
         </header>
       </div>
